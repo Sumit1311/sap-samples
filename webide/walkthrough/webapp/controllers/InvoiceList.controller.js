4b07b3feda338aef3990410a42a7725f0413d8fb
Amit Toshniwal <toshniwalami0@gmail.com>
2020-05-21 07:37:02 +0000

    Completed step 24 Walkthrough

--- webide/walkthrough/webapp/controllers/InvoiceList.controller.js
+++ webide/walkthrough/webapp/controllers/InvoiceList.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/demo/walkthrough/models/formatter"
-], function(Controller, JSONModel, formatter) {
+	"sap/ui/demo/walkthrough/models/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function(Controller, JSONModel, formatter, Filter, FilterOperator) {
 	return Controller.extend("sap.ui.demo.walkthrough.controllers.InvoiceList", {
 		formatter: formatter,
 		onInit: function(){
@@ -10,6 +12,11 @@ sap.ui.define([
 				currency: "EUR"	
 			});
 			this.getView().setModel(oViewModel, "view");
-		}	
+		},
+		onFilterInvoices: function(oEvent){
+			var query = oEvent.getParameter("newValue");
+			var listControl = this.byId("invoicesList");
+			listControl.getBinding("items").filter([new Filter("ProductName", FilterOperator.Contains, query)]);
+		}
 	});
 });
